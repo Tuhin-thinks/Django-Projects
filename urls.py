333dce7c7a779e9924fded5bfea9fd048754f1f4
Sipho123 <58542277+Sipho123@users.noreply.github.com>
2020-06-15 15:58:41 +0200

    changes made 15/06/2020
    path('new_member/', views.new_member),
    AttributeError: module 'bookings.views' has no attribute 'new_member'
    <input type="radio" class="btn" name="check" checked="checked"><span>Male</span>
    <input type="radio" class="btn" name="check"><span>Female</span>
    path('new_member/', views.forgot_password),
    Invalid block tag on line 1: 'extend'. Did you forget to register or load this tag?
       1 :  {% extend "login.html" %} 
    https://github.com/Sipho123/Django-Projects-2020.git
    ADMI PALNEL
    <a href="this PC.html"><select from the PC id="this PC"></select></a>
    added {% endblock %}
    rename the page as ADMINISTRATION PAGE

--- urls.py
+++ urls.py
@@ -2,12 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.home, name='login'), # this is for home
-    path('admin/', views.admin), # this is for admin panel
+    path('',views.login, name='login'), # this is for home
+     path('login/', views.login), # this is for login panel
+    path('administration/', views.administration), # this is for admin panel
     path('registration/', views.registration), # this is for registration now you have to add for login/
-    path('new_member/', views.registration)
-    path('forgot_password/', views.registration)
-    path('change_password/', views.registration)
+    path('new_member/', views.new_member),
+    path('forgot_password/', views.forgot_password),
+    path('change_password/', views.change_password),
 
 
     # these whole functions
--- views.py
+++ views.py
@@ -7,11 +7,20 @@ from django.http import HttpResponse
 def login(request):
     # if request.POST.get('click', False):
     #     self.registration(request)
-    # return render(request, 'bookings/home.html')
+    # return render(request, 'bookings/login.html')
     if request.method == 'GET':
         return render(request, 'bookings/login.html')
     elif request.method == 'POST':
         return render(request, 'bookings/login.html')
+
+def administration(request):
+    # if request.POST.get('click', False):
+    #     self.registration(request)
+    # return render(request, 'bookings/admin.html')
+    if request.method == 'GET':
+        return render(request, 'bookings/administration.html')
+    elif request.method == 'POST':
+        return render(request, 'bookings/administration.html')
 
 def registration(request):
     # if request.POST.get('click', False):
@@ -33,32 +42,32 @@ def admin(request):
         return render(request, 'bookings/admin.html')
     return render(request, 'bookings/admin.html') # /templates/booking/bookings/admin.html
 
-def new_menber(request):
+def new_member(request):
     # if request.POST.get('click', False):
     #     self.registration(request)
-    # return render(request, 'bookings/admin.html')
+    # return render(request, 'bookings/new_member.html')
     if request.method == 'GET':
         return render(request, 'bookings/new_member.html')
     elif request.method == 'POST':
         return render(request, 'bookings/new_member.html')
-    return render(request, 'bookings/new_member.html') # /templates/booking/bookings/new_menber.html
+    return render(request, 'bookings/new_member.html') # /templates/booking/bookings/new_member.html
 
 def forgot_password(request):
     # if request.POST.get('click', False):
     #     self.registration(request)
-    # return render(request, 'bookings/admin.html')
+    # return render(request, 'bookings/forgot_password.html')
     if request.method == 'GET':
-        return render(request, 'bookings/ forgot_password.html')
+        return render(request, 'bookings/forgot_password.html')
     elif request.method == 'POST':
-        return render(request, 'bookings/ forgot_password.html')
-    return render(request, 'bookings/ forgot_password.html') # /templates/booking/bookings/new_menber.html
+        return render(request, 'bookings/forgot_password.html')
+    return render(request, 'bookings/forgot_password.html') # /templates/booking/bookings/forgot_password.html
 
 def change_password(request):
     # if request.POST.get('click', False):
     #     self.registration(request)
-    # return render(request, 'bookings/admin.html')
+    # return render(request, 'bookings/change_password.html')
     if request.method == 'GET':
-        return render(request, 'bookings/ change_password.html')
+        return render(request, 'bookings/change_password.html')
     elif request.method == 'POST':
-        return render(request, 'bookings/ change_password.html')
-    return render(request, 'bookings/ change_password.html') # /templates/booking/bookings/new_menber.html
+        return render(request, 'bookings/change_password.html')
+    return render(request, 'bookings/change_password.html') # /templates/booking/bookings/change_password.html
