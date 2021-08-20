b232347189b5d50cb9ab9780cececef774993876
Aditya Maskar <aditya01092001@gmail.com>
2021-08-21 00:06:24 +0530

    Added SECRET_KEY and DEBUG to .env file
    for data protection I use python-deouple library (pip install python-couple ) in virtual env
    imported (from decouple import  config)
    created new file '.env ' and added  SECRET_KEY and Debug to it and you can see further in project yourself

--- PersonalBlog/PersonalBlog/settings.py
+++ PersonalBlog/PersonalBlog/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -19,10 +19,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)$b9gf%2)s((rwwu+0bx3(nnbn6_lh=1kt8(&$jza5pb(*rel4'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG')
 
 ALLOWED_HOSTS = []
 
